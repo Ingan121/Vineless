4735e782b5bc4e92d7be437613160cec6fde428f
Ingan121 <lssaa91192@gmail.com>
2025-08-31 20:43:22 +0900

    * Allow importing multiple devices at the same time * Devices of different types can be imported at the same time by using drag and drop or just select all files in the type selector of the file dialog

--- pages/picker/filePicker.js
+++ pages/picker/filePicker.js
@@ -6,53 +6,50 @@ const type = new URL(location.href).searchParams.get('type');
 const fileInput = document.getElementById('fileInput');
 fileInput.accept = type === "remote" ? ".json" : "." + type;
 
-fileInput.addEventListener('change', async (event) => {
-    try {
-        const file = event.target.files[0];
-        switch (type) {
-            case "wvd":
-                await SettingsManager.importDevice(file);
-                break;
-            case "remote":
-                await SettingsManager.loadRemoteCDM(file);
-                break;
-            case "prd":
-                await SettingsManager.importPRDevice(file);
-                break;
+async function importDevice(file) {
+    const importFunctions = {
+        "wvd": SettingsManager.importDevice,
+        "prd": SettingsManager.importPRDevice,
+        "remote": SettingsManager.loadRemoteCDM
+    };
+    // Always use this order, as prd validation is not strict
+    const order = ['remote', 'wvd', 'prd'];
+
+    for (const type of order) {
+        try {
+            await importFunctions[type](file);
+            console.log(`Imported ${file.name} as ${type}`);
+            return true;
+        } catch (e) {
+            console.warn(`Failed to import ${file.name} as ${type}:`, e);
+            // go on
         }
-        document.write("Imported successfully!"); // For stupid mobile browsers that window.close() doesn't work
-        window.close();
-    } catch (e) {
-        console.error(e);
-        window.resizeTo(800, 600);
-        alert("Invalid device file selected!");
     }
+    // real failure
+    return false;
+}
+
+fileInput.addEventListener('change', async (event) => {
+    for (const file of event.target.files) {
+        if (!await importDevice(file)) {
+            window.resizeTo(800, 600);
+            alert("Failed to import device file:", file.name);
+        }
+    }
+    document.write("Imported successfully!"); // For stupid mobile browsers that window.close() doesn't work
+    window.close();
 });
 
 document.addEventListener("drop", async (event) => {
     event.preventDefault();
-    const file = event.dataTransfer.files[0];
-    if (file) {
-        try {
-            switch (type) {
-                case "wvd":
-                    await SettingsManager.importDevice(file);
-                    break;
-                case "remote":
-                    await SettingsManager.loadRemoteCDM(file);
-                    break;
-                case "prd":
-                    await SettingsManager.importPRDevice(file);
-                    break;
-            }
-            document.write("Imported successfully!");
-            window.close();
-        } catch (e) {
-            console.error(e);
+    for (const file of event.dataTransfer.files) {
+        if (!await importDevice(file)) {
             window.resizeTo(800, 600);
-            alert("Invalid device file selected!");
+            alert("Failed to import device file:", file.name);
         }
     }
+    document.write("Imported successfully!"); // For stupid mobile browsers that window.close() doesn't work
+    window.close();
 });
 window.addEventListener("dragover", e => e.preventDefault());
 
@@ -62,16 +59,9 @@ document.getElementById('urlImport').addEventListener('click', async () => {
         const res = await fetch(url);
         const blob = await res.blob();
         blob.name = decodeURIComponent(url.split('/').pop());
-        switch (type) {
-            case "wvd":
-                await SettingsManager.importDevice(blob);
-                break;
-            case "remote":
-                await SettingsManager.loadRemoteCDM(blob);
-                break;
-            case "prd":
-                await SettingsManager.importPRDevice(blob);
-                break;
+        if (!await importDevice(blob)) {
+            window.resizeTo(800, 600);
+            alert("Failed to import!");
         }
         document.write("Imported successfully!");
         window.close();
